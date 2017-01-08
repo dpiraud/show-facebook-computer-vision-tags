aa5c44c12e4f0e3ece963b869768f67b960c5870
piraudev <piraudev@gmail.com>
2017-01-08 18:55:53 +0100

    Remove emoticon handling, adapt for french and add timer to ensure tag display

--- facebook.js
+++ facebook.js
@@ -1,82 +1,6 @@
-const emoji_map = {
-  "1 person": "👤",
-  "2 people": "👥",
-  "3 people": "👥",
-  "4 people": "👥",
-  "5 people": "👥",
-  "6 people": "👥",
-  "7 people": "👥",
-  "baby": "🍼",
-  "beach": "🏖",
-  "beard": "👴",
-  "bedroom": "🛏",
-  "bicycle": "🚲",
-  "camera": "📷",
-  "car": "🚗",
-  "cat": "😺",
-  "child": "👦",
-  "christmas tree": "🎄",
-  "close-up": "👀",
-  "closeup": "👀",
-  "cloud": "☁️",
-  "concert": "🎤",
-  "crowd": "👥",
-  "dancing": "💃",
-  "dessert": "🍰",
-  "dog": "🐶",
-  "drink": "🍹",
-  "eating": "🍽",
-  "eyeglasses": "👓",
-  "fireworks": "🎆",
-  "flower": "🌻",
-  "food": "🍎",
-  "glasses": "🕶",
-  "golf": "🏌️‍",
-  "grass": "🍃",
-  "hat": "👒",
-  "indoor": "🏠",
-  "living room": "🏠",
-  "meme": "👍",
-  "mountain": "🌋",
-  "nature": "🏞",
-  "night": "🌃",
-  "ocean": "🌊",
-  "office": "💼",
-  "one or more people": "👥",
-  "outdoor": "🚵",
-  "people eating": "🍽",
-  "people on stage": "🎤",
-  "people playing musical instruments": "🎸",
-  "people playing sport": "🏀",
-  "people sitting": "⑁",
-  "people smiling": "😂",
-  "people standing": "🕴",
-  "phone": "📱",
-  "plant": "🌿",
-  "playing a musical instrument": "🎸",
-  "selfie": "🤳",
-  "shoes": "👡",
-  "sitting": "⑁",
-  "sky": "☀️",
-  "skyscraper": "🏙",
-  "sleeping": "😴",
-  "smiling": "😋",
-  "snow": "❄️",
-  "standing": "🕴",
-  "stripes": "📶",
-  "suit": "🕴",
-  "sunglasses": "🕶",
-  "swimming": "🏊",
-  "table": "🍽",
-  "text": "🔠",
-  "tree": "🌴",
-  "twilight": "🌃",
-  "water": "💧",
-  "wedding": "💒"
-}
-
 const show_facebook_cv_tags = function() {
-  const TAG_PREFIX = "Image may contain: ";
+  //use part of prefix toi avoid issues with spécial char...
+  const TAG_PREFIX = "image contient peut";
   const images = [...document.getElementsByTagName('img')];
 
   images.forEach(function(el) {
@@ -86,19 +10,16 @@ const show_facebook_cv_tags = function() {
     el.setAttribute("data-prev-alt", el.alt);
 
     const altText = el.alt;
-    const isCVTag = altText.startsWith(TAG_PREFIX);
+    //Use indexOf instead of startsWith to avoid issues with spécial char..
+    const isCVTag = altText.indexOf(TAG_PREFIX) !== -1;
 
     if (isCVTag) {
-      const tags = altText.slice(TAG_PREFIX.length).split(/, | and /);
-      let html = "<ul style='position:absolute;top:10px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:rgba(0,0,0,0.7);color:#fff;border-radius:5px'>";
+      const tags = altText.slice(altText.indexOf(':')+1).split(/, | et /);
+      let html = "<ul style='position:absolute;top:10px;right:10px;padding:5px;font-size:12px;line-height:1.8;background-color:rgba(0,0,0,0.7);color:#fff;border-radius:5px;text-align:left'>";
 
       tags.forEach(function(tag){
-        let prefix = "∙";
-
-        if (tag in emoji_map)
-          prefix = emoji_map[tag];
-
-        html += `<li>${prefix} ${tag}</li>`;
+        let prefix = "&#9755;";
+        html += `<li>${prefix} ${tag.trim()}</li>`;
       });
 
       html += "</ul>";
@@ -119,4 +40,11 @@ const config = { attributes: true, childList: true, characterData: false }
 
 observer.observe(document.body, config);
 
-show_facebook_cv_tags();
+//Add timer to ensure tag refresh during navigation
+//it's sunday evening,I just want to make it work :-)
+const timer = function(){
+  show_facebook_cv_tags();
+  setTimeout(timer, 750)
+}
+
+timer();
